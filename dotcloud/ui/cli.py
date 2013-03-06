58242271b65a8a5e246816dd6be31fa43b5bd45c
INADA Naoki <inada-n@klab.com>
2013-03-06 16:17:34 +0900

    Fix auth.

--- dotcloud/ui/cli.py
+++ dotcloud/ui/cli.py
@@ -118,7 +118,7 @@ class CLI(object):
             self.info_output = sys.stderr
             p = get_parser(self.cmd)
             args = p.parse_args(args)
-            if args.debug is True:
+            if args.debug:
                 self.debug = True
                 self.client.debug = True
             if not self.global_config.loaded and args.cmd != 'setup':
