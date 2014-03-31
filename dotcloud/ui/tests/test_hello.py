22067d875005b9ca0d439c889218e3f37ee0e35e
Marc Abramowitz <marc@marc-abramowitz.com>
2014-03-31 11:55:13 -0700

    Fix broken test, add support for tox and Travis CI

--- dotcloud/ui/tests/test_hello.py
+++ dotcloud/ui/tests/test_hello.py
@@ -1,5 +1,12 @@
-from dotcloud.cli2 import CLI
+import os
+
+import pytest
+
+from dotcloud.ui import CLI
+
 
 def test_hello():
-    CLI().run()
-    assert True
+    url = os.environ.get('DOTCLOUD_API_ENDPOINT', 'https://ws.dotcloud.com/1.0')
+    cli = CLI(endpoint=url)
+    with pytest.raises(SystemExit):
+        cli.run(['--version'])
